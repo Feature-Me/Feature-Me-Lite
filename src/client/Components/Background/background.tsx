fb5c3525051d22d484d79fec85e4e4623fc526ee
Mksk <81460622+Mkskdesu@users.noreply.github.com>
2022-11-24 17:07:15 +0900

    modified background

--- src/client/Components/Background/background.tsx
+++ src/client/Components/Background/background.tsx
@@ -3,13 +3,12 @@ import React from "react";
 import style from "./background.scss"
 
 import backgroundImage from "Assets/Images/tidal wreck-AO.png";
+import GlitchImage from "Components/glitchImage/glitchImage";
 
 const Background: React.FC<{ onload?: Function }> = (props) => {
 
     return (
-        <div className={style.backgroundvanvas}>
-            <img src={backgroundImage} className={style.backgroundimg} />
-        </div>
+        <GlitchImage src={backgroundImage} className={style.backgroundimg} />
     )
 }
 
