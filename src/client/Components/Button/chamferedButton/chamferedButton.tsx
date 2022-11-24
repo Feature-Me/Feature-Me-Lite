fe61ecc2be9404b551a050cbdbe11568eccdf762
Mksk <81460622+Mkskdesu@users.noreply.github.com>
2022-11-24 17:07:33 +0900

    updated props type of button

--- src/client/Components/Button/chamferedButton/chamferedButton.tsx
+++ src/client/Components/Button/chamferedButton/chamferedButton.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 import style from "./chamferedButton.scss";
 
-type buttonPropsType = JSX.IntrinsicElements["button"] & {
+interface buttonPropsType extends React.InsHTMLAttributes<HTMLButtonElement> {
     accentColor?: string
 }
 
