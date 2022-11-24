00bbbde6f4d44b7816d9f81c975004728be171eb
Mksk <81460622+Mkskdesu@users.noreply.github.com>
2022-11-24 17:06:55 +0900

    added glitch image component

---
+++ src/client/Components/glitchImage/glitchImage.tsx
@@ -0,0 +1,22 @@
+//inspired by https://zenn.dev/ixkaito/articles/css-image-glitch
+
+import React from "react";
+
+import style from "./glitchImage.scss";
+
+interface GlitchImageProps extends React.InsHTMLAttributes<HTMLDivElement> {
+    src: string
+}
+
+const GlitchImage: React.FC<GlitchImageProps> = (props) => {
+
+    return (
+        <div className={style.glitch} style={{ backgroundImage: `url(${props.src})` }} {...props}>
+            <div className={`${style.channel} ${style.r}`}></div>
+            <div className={`${style.channel} ${style.g}`}></div>
+            <div className={`${style.channel} ${style.b}`}></div>
+        </div>
+    )
+}
+
+export default GlitchImage;
