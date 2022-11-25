e5817aadc2c809228a4867e191b605142c4b5bd4
Mksk <81460622+Mkskdesu@users.noreply.github.com>
2022-11-26 01:47:22 +0900

    updated home page

--- src/client/Pages/home/home.tsx
+++ src/client/Pages/home/home.tsx
@@ -1,4 +1,5 @@
 import FloatHeader from "Components/floatHeader/floatHeader";
+import { motion } from "framer-motion";
 import React from "react";
 import { useNavigate } from "react-router";
 
@@ -8,11 +9,23 @@ const Home:React.FC = () =>{
 
     const navigate  = useNavigate();
 
+    const init = {
+        opacity: 0,
+        y: 15
+    }
+    const animaiton = {
+        opacity: 1,
+        y: 0,
+        transition: {
+            duration: 0.2
+        }
+    }
+
     return(
-        <div className={style.home}>
+        <motion.div className={style.home} initial={init} animate={animaiton}>
             <FloatHeader title="Home" backFunc={()=>navigate("/title")} />
-            
-        </div>
+
+        </motion.div>
     )
 }
 
