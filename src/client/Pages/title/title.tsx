960193713718974702684595996811ee11c97a81
Mksk <81460622+Mkskdesu@users.noreply.github.com>
2022-11-25 19:09:13 +0900

    updated title screen

--- src/client/Pages/title/title.tsx
+++ src/client/Pages/title/title.tsx
@@ -1,30 +1,67 @@
 import React from "react";
+import { animations, motion, useAnimation } from "framer-motion";
 
 import version from "Config/versions.json";
 
 import style from "./title.scss";
-import { motion } from "framer-motion";
-import { opacity } from "html2canvas/dist/types/css/property-descriptors/opacity";
+
+import clickSound from "Assets/Sounds/click.mp3";
+import { Howl } from "howler";
+import { useNavigate } from "react-router";
+import sleep from "Utils/sleep/sleep";
 
 const Title: React.FC = () => {
+    const navigate = useNavigate();
+
+    const animationController =  useAnimation();
+
+    const audio = new Howl({
+        src: clickSound,
+        volume: 0.5,
+        loop: false,
+        autoplay: false,
+    });
 
     const init = {
-        opacity:0,
-        y:50
+        opacity: 0,
+        y: 15
     }
     const animaiton = {
-        opacity:1,
-        y:0,
-        transition:{
-            duration:0.2
+        opacity: 1,
+        y: 0,
+        transition: {
+            duration: 0.2
         }
     }
 
+    const exit = {
+        opacity: 0,
+        transition: {
+            duration: 0.2
+        }
+    }
+
+    async function clickTitle() {
+        audio.play();
+        animationController.start(exit)
+        await sleep(1000);
+        navigate("../load");
+
+    }
+
+    React.useEffect(()=>{
+        animationController.start(animaiton);
+        return()=>{
+            audio.unload();
+        }
+    },[])
+
     return (
-        <motion.div className={style.title} initial={init} animate={animaiton}>
+        <motion.div className={style.title} initial={init} animate={animationController} onClick={clickTitle} exit={exit}>
+            <div></div>
             <div className={style.titleText}>
                 <h1>Feature Me</h1>
-                <p>Click to start</p>
+                <p>Click to begin</p>
             </div>
             <div className={style.footer}>
                 <p>
