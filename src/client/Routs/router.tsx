9d5f1387f9c3e360c5ef9730305f4b53dfd882e5
Mksk <81460622+Mkskdesu@users.noreply.github.com>
2022-11-26 01:35:24 +0900

    updated router

--- src/client/Routs/router.tsx
+++ src/client/Routs/router.tsx
@@ -1,5 +1,6 @@
 import { AnimatePresence } from "framer-motion";
 import GameLoader from "Pages/gameLoader/gameLoader";
+import Home from "Pages/home/home";
 import Initializer from "Pages/Initializer/initializer";
 import SplashScreen from "Pages/splash/splashScreen";
 import Title from "Pages/title/title";
@@ -9,17 +10,15 @@ import { Routes, Route } from "react-router-dom";
 
 
 const PageRouter: React.FC = () => {
-    
+
     return (
-        <AnimatePresence>
-            <Routes>
-                <Route path="/" element={<Initializer />} />
-                <Route path="/title" element={<Title />} />
-                <Route path="/splash" element={<SplashScreen />} />
-                <Route path="/load" element={<GameLoader />} />
-                <Route path="/play/*" element={<></>} />
-            </Routes>
-        </AnimatePresence>
+        <Routes>
+            <Route path="/" element={<Initializer />} />
+            <Route path="/title" element={<Title />} />
+            <Route path="/splash" element={<SplashScreen />} />
+            <Route path="/load" element={<GameLoader />} />
+            <Route path="/home" element={<Home />} />
+        </Routes>
     )
 }
 
